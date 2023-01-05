01b8709e61febb74023d41388da274311c925691
soniapop93 <popsonia85@gmail.com>
2023-01-06 01:07:08 +0200

    Day 4 finished, both parts

---
+++ Day_4/day_4_1.py
@@ -0,0 +1,17 @@
+import hashlib
+
+input_string = "bgvyzdsv"
+
+
+def find_hash_value(input_string):
+    counter = 0
+    while True:
+        value_to_check = input_string + str(counter)
+        hash_string = hashlib.md5(value_to_check.encode('utf-8')).hexdigest()
+
+        if "00000" in hash_string[:5]:
+            return counter
+        counter += 1
+
+
+print(find_hash_value(input_string))
